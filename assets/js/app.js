44f22ba707b198fdc4ea05a678fea3cfbefaf1e5
Mohamed Ali <mohamedkosol@gmail.com>
2019-06-11 20:16:38 -0500

    Updated TS

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,65 +1,142 @@
-var firebaseConfig = {
-  apiKey: "AIzaSyCVxRG5rBQ8aGbAW7ROgy0-R4AsWq36tbg",
-  authDomain: "trainscheduler2636.firebaseapp.com",
-  databaseURL: "https://trainscheduler2636.firebaseio.com",
-  projectId: "trainscheduler2636",
-  storageBucket: "trainscheduler2636.appspot.com",
-  messagingSenderId: "344580793094",
-  appId: "1:344580793094:web:d4d29d48f5a43a59"
+var config = {
+  apiKey: "AIzaSyBP4S7UkLYvhQNWi4Y58Fw4NdIqHgsgv7k",
+  authDomain: "train-19919.firebaseapp.com",
+  databaseURL: "https://train-19919.firebaseio.com",
+  projectId: "train-19919",
+  storageBucket: "",
+  messagingSenderId: "206589574811",
+  appId: "1:206589574811:web:11a50d0ba0bad013"
 };
 
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(config);
 
-  var database = firebase.database();
-  
-      
+// Create a variable to reference the database.
+var database = firebase.database();
 
-  $("#submit-button").on("click", function (event) {
-   event.preventDefault();
+// initial values
+var trainName ="";
+var destination ="";
+var firstTrainTime ="3:30";
+ var frequency=0;
 
-    // I need grab the information from the form
-  var trainName =  $("#trainName").val().trim();
+
+
+$("#submit-button").on("click", function (event) {
+  event.preventDefault();
+
+  // I need grab the information from the form
+  var trainName = $("#trainName").val().trim();
   var destination = $("#trainDestination").val().trim();
   var firstTrainTime = $("#trainFirstTime").val().trim();
   var frequency = $("#trainFrequency").val().trim();
 
 
-  var newTrain = {
-  trainname: trainName,
-  destination: destination,
-  firsttime: firstTrainTime,
-  frequency: frequency,
-  dateAdded: firebase.database.ServerValue.TIMESTAMP
-  };
+  // var newTrain = {
+  //   trainname: trainName,
+  //   destination: destination,
+  //   firsttime: firstTrainTime,
+  //   frequency: frequency,
+  //   dateAdded: firebase.database.ServerValue.TIMESTAMP
+  // };
 
-  database.ref().push(newTrain);
+  database.ref().push({
+    trainname: trainName,
+    destination: destination,
+    firsttime: firstTrainTime,
+    frequency: frequency,
+    dateAdded: firebase.database.ServerValue.TIMESTAMP
+  });
 
-    
+
+  console.log(newTrain);
+
+
+
 });
 
-//  //Firebase watcher .on("child_added"
-//    firebase.database().ref().on("value", function(snapshot) {
-//         // storing the snapshot.val() in a variable for convenience
-//         var sv = snapshot.val();
-      
+database.ref().on("child_added", function (snapshot) {
+  var snapval = snapshot.val();
+  var arrival = moment(snapval.firsttime, "hh:mm").subtract(1, "years");
+  var diffTime = moment().diff(moment(arrival), "minutes");
+  var remainder = parseInt(diffTime % snapval.frequency);
+  // Minutes until next train
+  var minAway = parseInt(snapval.frequency) - remainder;
+  // Calculate next train time
+  var nextTrain = moment().add(minAway, "minutes");
+  // Convert minute to hour and minute format
+  nextTrain = moment(nextTrain).format("hh:mm A");
 
-//         // Console.loging the last user's data
-//         console.log(sv.trainname);
-//         console.log(sv.destination);
-//         console.log(sv.firsttime);
-//         console.log(sv.frequency);
-        
+  // console.log("THis is the minute away: "+ minAway)
+  // console.log("THis is remain "+ remainder)
+  // console.log("this is arrival"+ arrival)
+  // console.log("this is frequency"+ frequency)
+  // console.log("this is nextTrain"+ nextTrain)
+  // console.log("this is diffTime"+ diffTime)
 
-//         // Change the HTML to reflect
-//         $("#nameDisplay").html(snapshot.val)(sv.trainname);
-//         $("#destinationDisplay").html(snapshot.val)(sv.destination);
-//         $("#frequencyDisplay").html(snapshot.val)(sv.frequency);
-//         $("#firsttimeDisplay").html(snapshot.val)(sv.firsttime);
-//         $("#minutesDisplay").html(snapshot.val)(sv.minutes-away);
+  // Create the new row
+var newRow = $("<tr>").append(
+  $("<td>").text(snapval.trainname),
+  $("<td>").text(snapval.destination),
+  $("<td>").text(snapval.frequency),
+  $("<td>").text(nextTrain),
+  $("<td>").text(minAway)
+);
+console.log(newRow)
+// Append the new row to the table
+$("#train-table > tbody").append(newRow);
+$("#train-data").append(newRow);
+});
 
 
-//         // Handle the errors
-//       }, function(errorObject) {
-//         console.log("Errors handled: " + errorObject.code);
-//       });
+  
+
+  // var trainName = sv.trainname;
+  // var destination = sv.destination;
+  // var firstTrainTime = sv.firsttime;
+  // var frequency = sv.frequency;
+
+
+ 
+
+ 
+
+//    debugger
+
+// Console.loging the last user's data
+   
+// console.log(sv.trainname);
+// console.log(sv.destination);
+// console.log(sv.firsttime);
+// console.log(sv.frequency);
+  
+
+
+
+firebase.database().ref().on("value",function(snapshot){
+
+     //   // Change the HTML to reflect
+  $("#nameDisplay").html(snapshot.val)(sv.trainname);
+   $("#destinationDisplay").html(snapshot.val)(sv.destination);
+ $("#frequencyDisplay").html(snapshot.val)(sv.frequency);         
+ $("#firsttimeDisplay").html(snapshot.val)(sv.firsttime);
+   $("#minutesDisplay").html(snapshot.val)(sv.minutes-away);
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //         // Handle the errors
+//         function(errorObject) {
+//          console.log("Errors handled: " + errorObject.code);
+//        });
